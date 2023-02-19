838811458ad3cee2b5ca5682b788dcbef6945718
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2023-02-19 13:31:27 -0800

    Updating TODOs

--- src/main/ui/ClosetModeConsole.java
+++ src/main/ui/ClosetModeConsole.java
@@ -10,18 +10,21 @@ import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-// TODO
+// A console command system for a single closet
 public final class ClosetModeConsole extends CommandSystem {
 
     private final Closet closet;
 
-    // TODO
+    // EFFECTS: Creates a new closet mode console with the given
+    //          input stream and closet.
     public ClosetModeConsole(Scanner input, Closet closet) {
         super(input);
         this.closet = closet;
         this.run();
     }
 
+    // MODIFIES: this
+    // EFFECTS: Initializes the command system
     @Override
     protected void init() {
         this.setShouldRun(true);
@@ -30,21 +33,22 @@ public final class ClosetModeConsole extends CommandSystem {
         this.initCommands();
     }
 
+    // EFFECTS: Prompts for input for the next command and returns it.
     @Override
     protected String promptInput() {
         return this.getInput("Enter a closet command \""
                 + closet.getName() + "\" (ex. \"help\"): ");
     }
 
-    // Modifies: this
-    // Effects: Sets this command system to close
+    // MODIFIES: this
+    // EFFECTS: Sets this command system to close
     private void exit() {
         this.setShouldRun(false);
         System.out.println("\tExiting closet mode for \""
                 + this.closet.getName() + "\".");
     }
 
-    // Effects: Prints the types of clothing present in this closet
+    // EFFECTS: Prints the types of clothing present in this closet
     private void listTypes() {
         if (this.closet.getTypes().isEmpty()) {
             System.out.println("\tNo types in closet.");
@@ -58,7 +62,7 @@ public final class ClosetModeConsole extends CommandSystem {
     // Effects: searches this closet with the clothing address parsed
     //          from the user
     private void clothingAddressSearch() {
-        String input = this.getInput("Enter clothing addres expression: ");
+        String input = this.getInput("Enter clothing address expression: ");
         ClothingAddress address = null;
         try {
             address = ClothingAddress.of(input);
@@ -68,30 +72,20 @@ public final class ClosetModeConsole extends CommandSystem {
         }
         List<Clothing> clothing = this.closet.findClothing(address);
         if (clothing.isEmpty()) {
-            System.out.println("No clothing matched the given expression.");
+            System.out.println("\tNo clothing matched the given expression.");
         } else {
             System.out.println("Matches: \n" + clothing);
         }
     }
 
     // Modifies: this
-    // Effects: guides the user through a clothing creation process
+    // Effects: Guides the user through a clothing creation process.
     private void createClothing() {
-        String input = this.getInput("Enter a clothing type to create: ")
+        String input = this.getInput("\tEnter a clothing type to create: ")
                 .toLowerCase();
-        Clothing newClothing;
-        switch (input) {
-            case "shirt":
-                newClothing = this.createClothing(List.of("shirt"),
-                        null, null, null, null, null,
-                        null);
-                break;
-            default:
-                newClothing = this.createClothing(List.of(input),
-                        null, null, null, null, null,
-                        null);
-                break;
-        }
+        Clothing newClothing = this.createClothing(List.of(input),
+                null, null, null, null, null,
+                null);
         if (Objects.isNull(newClothing)) {
             return;
         }
@@ -107,13 +101,13 @@ public final class ClosetModeConsole extends CommandSystem {
                                     List<String> styles,
                                     Boolean dirty,
                                     Image image) {
-        if (Objects.isNull(types = this.parseStringList("Enter clothing types", types))
-                || Objects.isNull(size = this.parseEnumType("Enter size or type \"exit\": ", Size.class, size))
+        if (Objects.isNull(types = this.parseStringList("\tEnter clothing types", types))
+                || Objects.isNull(size = this.parseEnumType("\tEnter size or type \"exit\": ", Size.class, size))
                 || Objects.isNull(brand = this.parseString("brand", brand))
                 || Objects.isNull(material = this.parseString("material", material))
                 || Objects.isNull(styles
-                = this.parseStringList("Enter a list of styles (ex. \"street, skate\")", styles))
-                || Objects.isNull(dirty = this.parseBoolean("Enter whether the clothing is dirty", dirty))) {
+                = this.parseStringList("\tEnter a list of styles (ex. \"street, skate\")", styles))
+                || Objects.isNull(dirty = this.parseBoolean("\tEnter whether the clothing is dirty", dirty))) {
             System.out.println("Cancelled.");
             return null;
         }
@@ -129,7 +123,9 @@ public final class ClosetModeConsole extends CommandSystem {
         return t;
     }
 
-    // TODO
+    // EFFECTS: If the input boolean is not null, returns it. Prompts the user
+    //          for some boolean input and returns true or false depending on
+    //          the user input.
     private Boolean parseBoolean(String prompt, Boolean bool) {
         return ifNull(bool, () -> {
             String in = this.getInput(prompt + ", \"yes\" or \"no\": ");
@@ -142,7 +138,7 @@ public final class ClosetModeConsole extends CommandSystem {
         });
     }
 
-    // TODO
+    // EFFECTS: Formats a list of enum types in the given enum class to a string.
     private static <T extends Enum<T>> String formatEnumTypes(Class<T> enumClass) {
         return "Types: "
                 + Arrays.stream(enumClass.getEnumConstants())
@@ -150,7 +146,9 @@ public final class ClosetModeConsole extends CommandSystem {
                 .collect(Collectors.joining("\n\t - "));
     }
 
-    // TODO
+    // EFFECTS: Prompts the user for an enum input, parses it from a string, and returns the
+    //          enum value. If there is no corresponding value, returns null. If the input type
+    //          is not null, returns it.
     private <T extends Enum<T>> T parseEnumType(String prompt, Class<T> enumClass, T type) {
         return ifNull(type, () -> {
             String typeInput = this.getInputTrimOnly(formatEnumTypes(enumClass)
@@ -164,10 +162,12 @@ public final class ClosetModeConsole extends CommandSystem {
         });
     }
 
-    // TODO
+    // EFFECTS: If the given string is null, prompts the user for a string
+    //          input and returns it unless the string is "exit", in which
+    //          case returns null. If the given string is not null, returns it.
     private String parseString(String prompt, String s) {
         return ifNull(s, () -> {
-            String str = this.getInput("Enter " + prompt + " or \"exit\": ");
+            String str = this.getInput("\tEnter " + prompt + " or \"exit\": ");
             if (str.equalsIgnoreCase("exit")) {
                 return null;
             }
@@ -175,7 +175,8 @@ public final class ClosetModeConsole extends CommandSystem {
         });
     }
 
-    // TODO
+    // EFFECTS: If the given list types is null, prompts the user for a list of strings
+    //          and returns it. Otherwise, returns the input list.
     private List<String> parseStringList(String prompt, List<String> types) {
         return ifNull(types, () -> {
             String typesInput = this.getInput(prompt + " or type \"exit\": ")
@@ -189,26 +190,8 @@ public final class ClosetModeConsole extends CommandSystem {
         });
     }
 
-    // TODO
-    private <T extends Enum<T>> List<T> parseEnumList(String prompt,
-                                                      List<T> list,
-                                                      Class<T> enumClass) {
-        return ifNull(list, () -> {
-            String typeInput = this.getInput(formatEnumTypes(enumClass)
-                    + "\n" + prompt).toLowerCase();
-            return Arrays.stream(typeInput.split(""))
-                    .map(String::trim)
-                    .map(ti -> {
-                        try {
-                            return Enum.valueOf(enumClass, ti);
-                        } catch (IllegalArgumentException e) {
-                            System.out.println("Input did not match a given type.");
-                            return null;
-                        }
-                    }).collect(Collectors.toList());
-        });
-    }
-
+    // MODIFIES: this
+    // EFFECTS: Sets up the commands for this console interface.
     private void initCommands() {
         this.addCommands(
                 new ConsoleCommand(this::exit,
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,6 +1,8 @@
 package ui;
 
+// The application entry point
 public class Main {
+    // EFFECTS: Starts a new virtual closet application instance.
     public static void main(String[] args) {
         new VirtualClosetConsoleApp();
     }
